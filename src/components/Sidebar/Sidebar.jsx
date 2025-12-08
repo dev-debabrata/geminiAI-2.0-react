210db271a39a04a3a209c55b1c1f834c1987e200
Debabrata Das <debabratadas711@gmail.com>
2025-12-08 23:24:46 +0530

    08-12-2025

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -6,9 +6,9 @@ const Sidebar = () => {
     const [extended, setExtended] = useState(false)
 
     return (
-        <div className=' inline-flex flex-col justify-between min-h-[100vh] bg-[#f0f4f9] px-4 py-6'>
+        <div className=' inline-flex flex-col justify-between min-h-[100vh] bg-[#f0f4f9] px-3 py-6'>
             <div>
-                <Menu onClick={() => setExtended(prev => !prev)} size={20} className=' block ml-3 cursor-pointer' />
+                <Menu onClick={() => setExtended(prev => !prev)} size={20} className=' block ml-4 cursor-pointer' />
                 <div className=' mt-[50px] inline-flex items-center gap-2 py-3 px-4 bg-[#e6eaf1] rounded-full text-sm text-gray-500 cursor-pointer'>
                     <Plus size={28} />
                     {extended ? <p>New Chart</p> : null}
@@ -17,7 +17,7 @@ const Sidebar = () => {
                     ?
                     <div className=' flex flex-col'>
                         <p className=' mt-8 mb-5'>Recent</p>
-                        <div className=' flex items-start gap-2 px-4 py-3 pr-10 rounded-full text-[#282828] cursor-pointer hover:bg-[#e2e6eb]'>
+                        <div className=' flex items-start gap-2 px-4 py-3 rounded-full text-[#282828] cursor-pointer hover:bg-[#e2e6eb]'>
                             <MessageSquare />
                             <p>What is React ...</p>
                         </div>
@@ -26,15 +26,15 @@ const Sidebar = () => {
                 }
             </div>
             <div className=' flex flex-col'>
-                <div className='flex items-start gap-2 px-4 py-3 pr-10 rounded-full text-[#282828] cursor-pointer hover:bg-[#e2e6eb]'>
+                <div className='flex items-start gap-2 px-4 py-3 rounded-full text-[#282828] cursor-pointer hover:bg-[#e2e6eb]'>
                     <CircleQuestionMark />
                     {extended ? <p>Help</p> : null}
                 </div>
-                <div className='flex items-start gap-2 px-4 py-3 pr-10 rounded-full text-[#282828] cursor-pointer hover:bg-[#e2e6eb]'>
+                <div className='flex items-start gap-2 px-4 py-3 rounded-full text-[#282828] cursor-pointer hover:bg-[#e2e6eb]'>
                     <History />
                     {extended ? <p>Activity</p> : null}
                 </div>
-                <div className='flex items-start gap-2 px-4 py-3 pr-10 rounded-full text-[#282828] cursor-pointer hover:bg-[#e2e6eb]'>
+                <div className='flex items-start gap-2 px-4 py-3 rounded-full text-[#282828] cursor-pointer hover:bg-[#e2e6eb]'>
                     <Settings />
                     {extended ? <p>Settings</p> : null}
                 </div>
