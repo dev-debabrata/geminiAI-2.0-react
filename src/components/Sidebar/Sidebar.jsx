c63d48c6d67069a15ac7e32d951d403ca39bcc85
Debabrata Das <debabratadas711@gmail.com>
2025-12-08 21:53:06 +0530

    08-12-2025

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,38 @@
+import { Menu, MessageSquare, Plus, CircleQuestionMark, History, Settings } from 'lucide-react'
 import React from 'react'
 
 const Sidebar = () => {
     return (
-        <div>Sidebar</div>
+        <div>
+            <div>
+                <Menu />
+                <div>
+                    <Plus />
+                    <p>New Chart</p>
+                </div>
+                <div>
+                    <p>Recent</p>
+                    <div>
+                        <MessageSquare />
+                        <p>What is React ...</p>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <CircleQuestionMark />
+                    <p>Help</p>
+                </div>
+                <div>
+                    <History />
+                    <p>Activity</p>
+                </div>
+                <div>
+                    <Settings />
+                    <p>Settings</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
